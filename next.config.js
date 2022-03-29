30180521585c05bd8f0a717896f045390c9fe97b
juancho11gm <juansebastian11gm@gmail.com>
2022-03-28 21:03:09 -0500

    feat: getPlants from contentful
    feat: add hero, authors, and plantcollection

--- next.config.js
+++ next.config.js
@@ -2,10 +2,4 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
 
-const config = {
-  future: {
-    webpack5: true,
-  },
-}
-
-module.exports = withBundleAnalyzer(config)
+module.exports = withBundleAnalyzer()
